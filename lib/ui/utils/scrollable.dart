787835b835c779191373263cbdda11250c1e5223
Smart Precast NB2 <smartprecast.nb2@gmail.com>
2021-05-21 01:24:24 +0000

    added width and height as parameters

--- lib/ui/utils/scrollable.dart
+++ lib/ui/utils/scrollable.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Widget makeScrollable(Widget w) {
+Widget makeScrollable(Widget w, {double width = 2000.0, double height = 2000.0}) {
   return Scrollbar(
       child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: SizedBox(
-              width: 1000.0,
-              height: 1000,
+              width: width,
+              height: height,
               child: ListView(
                 children: [
                   SizedBox(
-                    width: 1000,
-                    height: 1000,
+                    width: width,
+                    height: height,
                     child: w,
                   )
                 ],
